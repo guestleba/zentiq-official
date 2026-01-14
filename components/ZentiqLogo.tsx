ebbd9813eed8de6704948af120668431d1b86a8e
LeoM <leonardomsilva10@gmail.com>
2026-01-14 16:27:56 +0000

    Adicionar arquivo de imagem do logo

--- components/ZentiqLogo.tsx
+++ components/ZentiqLogo.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import Image from 'next/image';
 
 export default function ZentiqLogo({ className }: { className?: string }) {
-  // Certifique-se de que o arquivo "icon.png" está na pasta "public"
   return (
-    <div className={`relative ${className}`}>
+    <div className={`${className} flex items-center justify-center`}>
       <Image 
         src="/icon.png" 
         alt="Zentiq Logo" 
-        fill 
-        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        className="object-contain"
+        width={100} 
+        height={100}
+        className="w-full h-full object-contain"
         priority
       />
     </div>
